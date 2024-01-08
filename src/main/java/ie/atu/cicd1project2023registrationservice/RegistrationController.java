23acf299e3fac7afb2cfac6cf879e6c96d2391ce
kennatu23 <g00267173@atu.ie>
2024-01-08 18:51:48 +0000

    Hellojoe

--- src/main/java/ie/atu/cicd1project2023registrationservice/RegistrationController.java
+++ src/main/java/ie/atu/cicd1project2023registrationservice/RegistrationController.java
@@ -1,17 +1,29 @@
 package ie.atu.cicd1project2023registrationservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegistrationController {
+    private NotifyClient notifyClient;
+    @Autowired
+    public RegistrationController(NotifyClient notifyClient) {
+        this.notifyClient = notifyClient;
+    }
 
     @PostMapping("/Verify")
     public String verifyDetails(@RequestBody PersonDetails personDetails) {
         String verificationMessage = String.format("Confirming %s email %s has purchased following purchase", personDetails.getName(), personDetails.getEmail());
         return verificationMessage;
 
+    }
+
+    public String notifyDetails(@RequestBody PersonDetails personDetails){
+
+        String notify = notifyClient.anyDetails(personDetails);
+        return notify;
     }
 }
 
