4d3435a886b1c98037c8b291365882cb4069dfb5
DanKos22 <G00397054@atu.ie>
2024-01-08 16:48:21 +0000

    First commit

---
+++ src/test/java/ie/atu/cicd1project2023registrationservice/Cicd1Project2023RegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd1project2023registrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd1Project2023RegistrationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
