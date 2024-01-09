5c67bb13baace1c21683f71055ea4f5d51a0418e
kennatu23 <g00267173@atu.ie>
2024-01-09 20:56:39 +0000

    Container4

--- src/main/java/ie/atu/cicd1project2023registrationservice/NotifyClient.java
+++ src/main/java/ie/atu/cicd1project2023registrationservice/NotifyClient.java
@@ -4,10 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "affirm", url = "http://localhost:8082")
+@FeignClient(name = "affirm", url = "http://notification-service:8082")
 public interface NotifyClient {
-
-
     @PostMapping("/subscribe")
     String anyDetails(@RequestBody PersonDetails personDetails);
 }
